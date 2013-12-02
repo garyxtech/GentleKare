df873812f5d12196a3a70a4d67147345fc2587ba
Gary Xue <garyx.tech@gmail.com>
2013-12-02 21:40:44 +0800

    Initial Commit

---
+++ Workspace/GentleKare/GentleKare/GKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GKAppDelegate.h
+//  GentleKare
+//
+//  Created by 薛洪 on 13-12-2.
+//  Copyright (c) 2013年 薛洪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Workspace/GentleKare/GentleKareTests/GentleKareTests.m
@@ -0,0 +1,34 @@
+//
+//  GentleKareTests.m
+//  GentleKareTests
+//
+//  Created by 薛洪 on 13-12-2.
+//  Copyright (c) 2013年 薛洪. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GentleKareTests : XCTestCase
+
+@end
+
+@implementation GentleKareTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
